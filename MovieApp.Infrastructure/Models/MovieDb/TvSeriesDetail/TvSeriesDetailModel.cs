14239ce92d95536d3fa81b370575cd5c24c430d9
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2024-09-29 00:42:33 +0300

    TcSeriesDetailModel Update

--- MovieApp.Infrastructure/Models/MovieDb/TvSeriesDetail/TvSeriesDetailModel.cs
+++ MovieApp.Infrastructure/Models/MovieDb/TvSeriesDetail/TvSeriesDetailModel.cs
@@ -46,38 +46,37 @@ namespace MovieApp.Infrastructure.Models.MovieDb.TvSeriesDetail
     public class TvSeriesDetailModel
     {
         public bool adult { get; set; }
-        //public string backdrop_path { get; set; }
-        //public List<object> created_by { get; set; }
-        //public List<int> episode_run_time { get; set; }
+        public string backdrop_path { get; set; }
+        public List<object> created_by { get; set; }
+        public List<int> episode_run_time { get; set; }
         //public string first_air_date { get; set; }
-        //public List<Genre> genres { get; set; }
+        public List<Genre> genres { get; set; }
         //public string homepage { get; set; }
         public int id { get; set; }
-        public string backdrop_path { get; set; }
         public bool in_production { get; set; }
-        //public List<string> languages { get; set; }
+        public List<string> languages { get; set; }
         //public string last_air_date { get; set; }
         //public LastEpisodeToAir last_episode_to_air { get; set; }
-        //public string name { get; set; }
+        public string name { get; set; }
         //public object next_episode_to_air { get; set; }
-        //public List<Network> networks { get; set; }
+        public List<Network> networks { get; set; }
         public int number_of_episodes { get; set; }
         public int number_of_seasons { get; set; }
-        //public List<string> origin_country { get; set; }
+        public List<string> origin_country { get; set; }
         public string original_language { get; set; }
         public string original_name { get; set; }
-        //public string overview { get; set; }
+        public string overview { get; set; }
         //public double popularity { get; set; }
-        //public string poster_path { get; set; }
+        public string poster_path { get; set; }
         //public List<object> production_companies { get; set; }
         //public List<ProductionCountry> production_countries { get; set; }
         //public List<Season> seasons { get; set; }
-        //public List<SpokenLanguage> spoken_languages { get; set; }
-        //public string status { get; set; }
-        //public string tagline { get; set; }
-        //public string type { get; set; }
+        public List<SpokenLanguage> spoken_languages { get; set; }
+        public string status { get; set; }
+        public string tagline { get; set; }
+        public string type { get; set; }
         //public double vote_average { get; set; }
-        //public int vote_count { get; set; }
+        public int vote_count { get; set; }
     }
 
     public class Season
