619c3bba23aad2cae41734ea27384b8ddf3ef751
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-01-08 23:19:35 +0300

    BaseEntity Add

---
+++ MovieApp.Domain/Entity/Base/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApp.Domain.Entity
+{
+    public class BaseEntity
+    {
+        [Column(Order = 0), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public long? CreatedById { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public long? ModifiedById { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int RecVersion { get; set; } = 0;
+        public long? DbTableId { get; set; }
+    }
+}
