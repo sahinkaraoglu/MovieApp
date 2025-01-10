8deb08bb89bd7830a85f4ea9efc36926d9824c6f
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-01-11 00:55:47 +0300

    Mg

---
+++ MovieApp.Infrastructure/Migrations/20250110215351_BeforeBaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class BeforeBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
